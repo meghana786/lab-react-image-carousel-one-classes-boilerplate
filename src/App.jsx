ca2e57781a2d95dc3cbd5a005f7aa993aa835343
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-05-21 17:01:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
-import Carousel from "./components/Carousel";
-import "./App.css";
+import React from 'react';
+import Carousel from './components/Carousel';
+import eyes from './images/eyes.jpg';
+import faces from './images/faces.jpg';
+import girlface from './images/girlface.jpg';
 
+const images = [
+  { src: eyes, alt: 'Eyes Image', text: 'Crying Lady, Why so serious?' },
+  { src: faces, alt: 'Faces Image', text: 'Faces, How many faces do you have?' },
+  { src: girlface, alt: 'Girl Face Image', text: 'Inside out, The best person to guide you is yourself' }
+];
 
+const App = () => {
+  return (
+    <div className="App">
+      <Carousel images={images} />
+    </div>
+  );
+};
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Carousel />
-      </div>
-    );
-  }
-}
+export default App;
